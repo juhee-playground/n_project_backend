fb832945e1cf1c3f66f36caa2e319ee9b7f57595
Cheol_Kim <cheol.kim@fitogether.com>
2020-01-11 16:58:58 +0900

    Make Log on Mysql Connection

--- custom_lib/db_connection.js
+++ custom_lib/db_connection.js
@@ -2,5 +2,13 @@ var mysql      = require('mysql');
 var dbconfig   = require('../config/database.js');
 var connection = mysql.createConnection(dbconfig);
 
+connection.connect((err)=>{
+    if(!err){
+        console.log("Mysql Connected!!");
+    }
+    else{
+        console.log("Mysql Connection Failed", err);
+    }
+})
 
 module.exports = connection;
