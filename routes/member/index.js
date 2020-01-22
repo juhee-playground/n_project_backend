7a14d4881515c9006d542d818909d366ea29c6ed
Juhee Baek <juhee.baek@fitogether.com>
2020-01-22 20:01:37 +0900

    member backend change

--- routes/member/index.js
+++ routes/member/index.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const connection = require("../../custom_lib/db_connection")
+var qs = require('querystring');
 
 const router = express.Router();
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
     res.send('Update Member World');
 });
 
 // Create Member
-router.post('/create', function(req, res){
+router.post('/create', function (req, res) {
     var memberData = req.body
     // memberData.created_at = new Date();
     console.log(memberData)
-    connection.query('INSERT INTO member SET ?', memberData, function(err,results, fields) {
-        if(err) {
-            console.error(err, results); 
+    connection.query('INSERT INTO member SET ?', memberData, function (err, results, fields) {
+        if (err) {
+            console.error(err, results);
             res.send(err, results);
         }
         console.log(results.insertId);
@@ -23,25 +24,36 @@ router.post('/create', function(req, res){
 });
 
 // Read Member
-router.get('/member', function(req, res){
-    connection.query('SELECT * from member', function(err, results, fields) {
-        if(err) {
-            res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-        }else{
-            res.send(JSON.stringify({"status":200, "error":null, "response": results}));
+router.get('/members/', function (req, res) {
+    connection.query('SELECT * from member', function (err, results, fields) {
+        if (err) {
+            res.send(JSON.stringify({
+                "status": 500,
+                "error": error,
+                "response": null
+            }));
+        } else {
+            res.send(results);
         }
     });
 });
 
-// Retrieve member with id 
-router.get('/member/:id', function (req, res) {
+// Retrieve member with id
+router.get('/members:id', function (req, res) {
     let member_id = req.params.id;
     if (!member_id) {
-        return res.status(400).send({ error: true, message: 'Please provide member_id' });
+        return res.status(400).send({
+            error: true,
+            message: 'Please provide member_id'
+        });
     }
     connection.query('SELECT * FROM member where id=?', member_id, function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results[0], message: 'members list.' });
+        return res.send({
+            error: false,
+            data: results[0],
+            message: 'members list.'
+        });
     });
 });
 
@@ -52,18 +64,26 @@ router.put('/update', function (req, res) {
     let member = req.body.member;
     console.log(member)
     if (!member_id || !member) {
-      return res.status(400).send({ error: member, message: 'Please provide member and member_id' });
+        return res.status(400).send({
+            error: member,
+            message: 'Please provide member and member_id'
+        });
     }
 
     connection.query("UPDATE member SET ? WHERE id = ?", [member, member_id], function (error, results, fields) {
-            if (error) throw error;
-            return res.send({ error: false, data: results, message: 'member has been updated successfully.' });
+        if (error) throw error;
+        return res.send({
+            error: false,
+            data: results,
+            message: 'member has been updated successfully.'
         });
     });
+});
 
 // Delete Member
-router.delete('/delete', function(req, res){
-    connection.query('DELETE FROM member WHERE id=?', [req.body.member_id], function (error, results, fields) {
+router.delete('/delete', function (req, res) {
+    var body = '';
+    connection.query('DELETE FROM member WHERE id=?', [req.params.member_id], function (error, results, fields) {
         if (error) throw error;
         res.send('Record has been deleted!');
     });
