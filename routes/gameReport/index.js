420e4a9ebf99d46542474e5a29321da684a59dd7
Cheol_Kim <cheol.kim@fitogether.com>
2020-06-13 18:34:07 +0900

    Clean up

--- routes/gameReport/index.js
+++ routes/gameReport/index.js
@@ -5,32 +5,37 @@ var qs = require("querystring");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-    res.send("Hello gameReport World");
+  res.send("Hello gameReport World");
 });
 
 // Create GameReport
 router.post("/create", function (req, res, next) {
-    var gameReportData = req.body;
+  var gameReportData = req.body;
 
-    console.log('Create GameReport', gameReportData);
-    connection.query("INSERT INTO gameReport SET ?", gameReportData, function (err, results, fields) {
-        if (err) next(err);
-        res.send(results);
-    });
+  console.log("Create GameReport", gameReportData);
+  connection.query("INSERT INTO gameReport SET ?", gameReportData, function (
+    err,
+    results,
+    fields
+  ) {
+    if (err) next(err);
+    res.send(results);
+  });
 });
 
 // Read GameReport
 router.get("/list", function (req, res, next) {
-    connection.query("SELECT * from gameReport", function (err, results, fields) {
-        if (err) next(err);
-        res.send(results);
-    });
+  connection.query("SELECT * from gameReport", function (err, results, fields) {
+    if (err) next(err);
+    res.send(results);
+  });
 });
 
 // Read selected GameReport
 router.get("/getinfo/:id", function (req, res, next) {
-    let gameReport_id = req.params.id;
-    connection.query("SELECT gameReport.*, gameReport.first_player, gameReport.last_player, \
+  let gameReport_id = req.params.id;
+  connection.query(
+    `SELECT gameReport.*, \
                             first_member.name as first_player_name, last_member.name as last_player_name, \
                             first_member.uniform_number as first_player_uniform_number, last_member.uniform_number as last_player_uniform_number \
                             from gameReport  \
@@ -50,47 +55,51 @@ router.get("/getinfo/:id", function (req, res, next) {
 
 // Read selected GameReport with game_id
 router.get("/getinfoWithGameId/:id", function (req, res, next) {
-    let game_id = req.params.id;
-    connection.query("SELECT * from gameReport where game_id = ?", game_id, function (err, results, fields) {
-        if (err) next(err);
-        res.send(results);
-    });
+  let game_id = req.params.id;
+  connection.query(
+    "SELECT * from gameReport where game_id = ?",
+    game_id,
+    function (err, results, fields) {
+      if (err) next(err);
+      res.send(results);
+    }
+  );
 });
 
 //  Update GameReport with id
 router.put("/update", function (req, res, next) {
-    console.log("GameReport Update", req.body);
-    let gameReport_id = req.body.gameReport_id;
-    let gameReport = req.body.gameReport;
+  console.log("GameReport Update", req.body);
+  let gameReport_id = req.body.gameReport_id;
+  let gameReport = req.body.gameReport;
 
-    if (!gameReport_id || !gameReport) {
-        return res.status(400).send({
-            err: gameReport,
-            message: "Please provide gameReport and gameReport_id"
-        });
+  if (!gameReport_id || !gameReport) {
+    return res.status(400).send({
+      err: gameReport,
+      message: "Please provide gameReport and gameReport_id"
+    });
+  }
+
+  connection.query(
+    "UPDATE gameReport SET ? WHERE id = ?",
+    [gameReport, gameReport_id],
+    function (err, results, fields) {
+      if (err) next(err);
+      res.send(results);
     }
-
-    connection.query(
-        "UPDATE gameReport SET ? WHERE id = ?",
-        [gameReport, gameReport_id],
-        function (err, results, fields) {
-            if (err) next(err);
-            res.send(results);
-        }
-    );
+  );
 });
 
 // Delete GameReport
 router.delete("/delete", function (req, res, next) {
-    console.log('Delete GameReport', req.body);
-    connection.query(
-        "DELETE FROM gameReport WHERE id=?",
-        [req.body.gameReport_id],
-        function (err, results, fields) {
-            if (err) next(err);
-            res.send(results);
-        }
-    );
+  console.log("Delete GameReport", req.body);
+  connection.query(
+    "DELETE FROM gameReport WHERE id=?",
+    [req.body.gameReport_id],
+    function (err, results, fields) {
+      if (err) next(err);
+      res.send(results);
+    }
+  );
 });
 
 module.exports = router;
