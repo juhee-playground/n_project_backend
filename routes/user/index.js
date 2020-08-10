5e82c688e8701b465b49af4bdb4549cd26a32c46
dmsghs92 <dmsghs92@gmail.com>
2020-08-11 03:16:17 +0900

    Add middleWare for Auth

--- routes/user/index.js
+++ routes/user/index.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const encrypt = require("../../config/encrypt");
 const secret = encrypt.secret;
 const router = express.Router();
+const authMiddleware = require("../../custom_lib/auth_middleware");
 
 router.get("/", function (req, res, next) {
   res.send("Update user World");
@@ -73,43 +74,12 @@ router.post("/login", function (req, res, next) {
   );
 });
 
+router.use("/check", authMiddleware);
 router.get("/check", function (req, res, next) {
-  // read the token from header or url
-  const token = req.headers["x-access-token"] || req.query.token;
-
-  // token does not exist
-  if (!token) {
-    res.status(403).json({
-      success: false,
-      message: "not logged in",
-    });
-  }
-  // create a promise that decodes the token
-  const p = new Promise((resolve, reject) => {
-    jwt.verify(token, secret, (err, decoded) => {
-      if (err) reject(err);
-      resolve(decoded);
-    });
+  res.json({
+    success: true,
+    info: req.decoded,
   });
-
-  // if token is valid, it will respond with its info
-  const respond = (token) => {
-    res.json({
-      success: true,
-      info: token,
-    });
-  };
-
-  // if it has failed to verify, it will return an error message
-  const onError = (error) => {
-    res.status(403).json({
-      success: false,
-      message: error.message,
-    });
-  };
-
-  // process the promise
-  p.then(respond).catch(onError);
 });
 
 // Delete Attend
