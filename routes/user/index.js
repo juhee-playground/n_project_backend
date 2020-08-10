418516b3cb73638a5df3917bc635b4df1447ddaf
Cheol_Kim <cheol.kim@fitogether.com>
2020-08-10 21:02:55 +0900

    Init

---
+++ routes/user/index.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const connection = require("../../custom_lib/db_connection");
+
+const jwt = require('jsonwebtoken')
+
+const router = express.Router();
+
+router.get("/", function (req, res, next) {
+  res.send("Update user World");
+});
+
+
+
+// Create Attend
+router.post("/register", function (req, res, next) {
+  var userData = req.body; // {name, password}
+
+  // create a new user if does not exist
+  console.log('Create User', userData);  
+  connection.query("INSERT INTO user SET ?", userData, function (err, results, fields) {
+    if (err) next(err);
+
+    // respond to the client
+    res.send(results);
+  });
+});
+
+// Create Attend
+router.post("/login", function (req, res, next) {
+  var userData = req.body; // {name, password}
+  // jwt 토큰 발생
+  const secret = "apple" // FIXME: 비밀키 외부노출이 되면 안됨 .gitignore에 있을 것 https://victorydntmd.tistory.com/116
+  const token = jwt.sign(
+    {"name":userData.name}, // 여기에 password가 들어가있으면 token인증시 password를 노출하게 된다 그러므로 넣지말자 
+    secret, 
+    {
+        expiresIn: '1d', // 하루 5m 은 5분
+        issuer: 'nnnn.com',
+        subject: 'userInfo'
+    })
+
+  connection.query(`SELECT * FROM user WHERE ?`, {'name': userData.name}, function (err, results, fields) {
+    
+    if (err) next(err);
+    
+    const result = JSON.parse(JSON.stringify(results)) // https://stackoverflow.com/questions/31221980/how-to-access-a-rowdatapacket-object
+    
+    if (result.length == 0) {
+      res.status(500).send("No Matching UserName");
+    }else if (result && result[0].password !==userData.password) {
+      res.status(500).send("Password is Wrong");
+    }else{
+      res.cookie("user", token);
+      res.send({token});
+    }
+  });
+});
+
+router.get("/check/:token",function(req,res, next){
+  // read the token from header or url 
+  const token = req.headers['x-access-token'] || req.params.token
+  const secret = "apple" // FIXME: 비밀키 외부노출이 되면 안됨 .gitignore에 있을 것 https://victorydntmd.tistory.com/116
+
+  // token does not exist
+  if(!token) {
+      res.status(403).json({
+          success: false,
+          message: 'not logged in'
+      })
+  }
+  // create a promise that decodes the token
+  const p = new Promise(
+    (resolve, reject) => {
+      jwt.verify(token, secret, (err, decoded) => {
+          if(err) reject(err)
+          resolve(decoded)
+      })
+    }
+  )
+
+  // if token is valid, it will respond with its info
+  const respond = (token) => {
+      res.json({
+          success: true,
+          info: token
+      })
+  }
+
+  // if it has failed to verify, it will return an error message
+  const onError = (error) => {
+      res.status(403).json({
+          success: false,
+          message: error.message
+      })
+  }
+
+  // process the promise
+  p.then(respond).catch(onError)
+
+})
+
+// Delete Attend
+router.delete("/delete", function (req, res, next) {
+  console.log('Delete attend', req.body);
+  connection.query(
+    `DELETE FROM attend WHERE member_id= ${req.body.member_id} and schedule_id = ${req.body.schedule_id}`,
+    function (err, results, fields) {
+      if (err) next(err);
+      res.send(results);
+    }
+  );
+});
+
+
+module.exports = router;
