a381973a6ee4c23cdd6ca2239163cc951f01c7da
Juhee <juhee.baek@fitogether.com>
2020-11-02 22:38:34 +0900

    유닛팀 멤버 삭제 api

--- routes/unitMember/index.js
+++ routes/unitMember/index.js
@@ -31,32 +31,36 @@ router.get("/list/:id", function(req, res, next) {
       message: "Please provide unit_team_id"
     });
   }
-  connection.query("SELECT unit_team_id, member_id, name, uniform_number, nick_name, join_date \
+  connection.query("SELECT id_unit_member, unit_team_id, member_id, name, uniform_number, nick_name, join_date \
                     FROM unitMember \
                     INNER JOIN member ON member.id = unitMember.member_id \
                     where unit_team_id = ?", unit_team_id, 
-  function(
-    err,
-    results,
-    fields
-  ) {
+  function( err, results, fields) {
     if (err) next(err);
-    console.log("results", results);
     if (results.length == 0) {
-      res.status(400).send({
-        err:true, 
-        message:"No Result Found"
-      }) 
+      return res.status(401).send({
+        err: true,
+        message: "no result"
+      });
     }
     res.send(results);
   });
 });
 
+router.get("/list", function(req, res, next) {
+  connection.query("SELECT * FROM unitMember",
+  function( err, results, fields) {
+    if (err) next(err);
+    console.log(results);
+    res.send(results);
+  });
+});
+
 // Delete Member
-router.delete("/delete", function(req, res, next) {
+router.delete("/delete/:id", function(req, res, next) {
   connection.query(
-    "DELETE FROM unitMember WHERE unit_team_id=?",
-    [req.body.unit_team_id],
+    "DELETE FROM unitMember WHERE id_unit_member=?",
+    [req.params.id],
     function(err, results, fields) {
       if (err) next(err);
       console.log(results);
